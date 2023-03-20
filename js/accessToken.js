cd75c62e5bb265a088c19caa5f76d03740cb7ef7
ozo-vehe <johnnie.vehe@gmail.com>
2023-03-21 00:06:38 +0100

    modified accessToken.js to request for new token

--- js/accessToken.js
+++ js/accessToken.js
@@ -1,3 +1,4 @@
+const CLIENT_ID = "89e5a59d75d74c3a98a357a8fcc664cb";
 // variable to hold token created after authentication
 let token;
 // get the hash from the url
@@ -5,7 +6,7 @@ const hash = window.location.hash;
 // if token is present in localStorage, get the token
 token = localStorage.getItem("token");
 // if token is not save in localStorage but a hash is gotten, get the token from the hash
-if (!token && hash) {
+if (token && hash) {
   token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
   // reset the hash to an empty string
   window.location.hash = ""
@@ -13,17 +14,35 @@ if (!token && hash) {
   window.localStorage.setItem("token", token)
 }
 
+console.log(token);
 // function to get the user logged in and check if authorization works
 const user = async() => {
+  let data;
   // create a new url using the spotify api with the user endpoint
   const url = new URL("https://api.spotify.com/v1/me");
   // using fetch api, get the details of the user logged in passing the token in the
   // localStorage as the header
-  const data = await fetch(url, {
+  data = await fetch(url, {
     headers: {
       Authorization: `Bearer ${token}`
     }
   });
+  // checks if access token has expired as access token generated has short expiration time
+  if(data.status === 401) {
+    // create a search params using new parameters to request for a new access token
+    url.search = new URLSearchParams({
+      grant_type: "refresh_token",
+      refresh_token: token,
+      client_id: CLIENT_ID
+    })
+    // fetch data again
+    data = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+  }
   // convert the data gotton to json format
   const jsonData = await data.json();
   // use aler to display the logged in user name for verification
