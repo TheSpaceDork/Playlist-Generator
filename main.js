0f922195a09fb17afba87120d0c969bce3c60056
TheSpaceDork <treasurechux@gmail.com>
2023-03-21 17:24:52 +0100

    added authorization

--- main.js
+++ main.js
@@ -4,21 +4,49 @@ const changeImage = (imgSrc) => {
   img.src = imgSrc;
   img.classList.add("scale");
 };
-const redirect_uri = "http://127.0.0.1:5500/stepone.html";
+const redirect_uri = "http://127.0.0.1:5500/steptwo.html";
 
-const client_id = "df032b522964736bfa5635b5f3e48e4";
+const client_id = "3dc5285458fa48759206134beca949c1";
 const client_secret = "e7460502b84441df83db78e3ae7f05e5";
 const AUTHORIZE = "https://accounts.spotify.com/authorize";
-
-const onPageLoad = () => {};
+const SCOPES = [
+  "playlist-modify-public",
+  "streaming",
+  "user-library-read",
+  "playlist-modify-private",
+  "user-read-currently-playing",
+  "playlist-read-private",
+  "user-modify-playback-state",
+  "user-read-playback-state",
+];
+const space_del = "%20";
+const scopesUrl = SCOPES.join(space_del);
 
 const requestAuthorization = () => {
-  let url = AUTHORIZE;
-  url += "?client_id" + client_id;
-  url += "&response_type=code";
-  url += "&redirect_uri" + encodeURI(redirect_uri);
-  url += "&show_dialog=true";
-  url +=
-    "&scope=user-read-private user-read-email user-modify-playback-state user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
-  window.location.href = url; //show spotify auth screen
+  window.location = `${AUTHORIZE}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopesUrl}&response_type=token&show_dialog=true`;
+  //show spotify auth screen
 };
+const getAccessTokenFromUrl = () => {
+  const hash = window.location.hash.substring(1);
+  const params = hash.split("&").reduce(function (acc, curr) {
+    const [key, value] = curr.split("=");
+
+    acc[key] = decodeURIComponent(value);
+    return acc;
+  }, {});
+  return params.access_token;
+};
+const accessToken = getAccessTokenFromUrl();
+const user = document.getElementById("user");
+
+fetch("https://api.spotify.com/v1/me", {
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    const currentUser = data.display_name;
+    user.textContent = currentUser;
+  })
+  .catch((error) => console.error(error));
